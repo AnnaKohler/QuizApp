504cd5a6204a3870a174f301ea3302e22c844fcc
AnnaKohler <anna.kohler.610@gmail.com>
2016-12-01 15:17:09 +0400

    Stage 2

--- app/src/main/java/ru/startandroid/myapplication/MainActivity.java
+++ app/src/main/java/ru/startandroid/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean ans=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
         btn1.setText("Да");
         Button btn2 =(Button)findViewById(R.id.btn2);
         btn2.setText("Нет");
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                findViewById(R.id.txtWrong).setVisibility(View.GONE);
+                findViewById(R.id.txtRight).setVisibility(View.GONE);
+
+              if(ans)findViewById(R.id.txtRight).setVisibility(View.VISIBLE);
+                else findViewById(R.id.txtWrong).setVisibility(View.VISIBLE);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.txtWrong).setVisibility(View.GONE);
+                findViewById(R.id.txtRight).setVisibility(View.GONE);
+                if(!ans)findViewById(R.id.txtRight).setVisibility(View.VISIBLE);
+                else findViewById(R.id.txtWrong).setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 }
