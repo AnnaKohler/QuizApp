abc19e51fb77df4b6803c0b38e8dfc854863789d
AnnaKohler <anna.kohler.610@gmail.com>
2016-12-01 13:13:18 +0400

    Initial commit

---
+++ app/src/main/java/ru/startandroid/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package ru.startandroid.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1=(Button)findViewById(R.id.btn1);
+        btn1.setText("Да");
+        Button btn2 =(Button)findViewById(R.id.btn2);
+        btn2.setText("Нет");
+    }
+
+}
